4b381bbf0a903054f74091c821cd95c09f73f7a9
bharatakind-debug <bharatakind@gmail.com>
2026-03-07 20:20:34 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Chatboy")
-user_input = st.chat_input("Say something...")
-
-if user_input:
-    st.write(f"You said: {user_input}")
-    st.write("Chatboy is listening!")
